b1a7b7d50d26889964693eaab9d0c00eb1832274
Ivan Trofimov <101204403+hanglider@users.noreply.github.com>
2024-12-15 18:45:29 +0300

    del rub files

--- fp/detector_with_GUI/blocker.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import IP, ICMP, send
-from config import BLOCKING_ENABLED
-
-def block_ip(ip_address):
-    if BLOCKING_ENABLED:
-        packet = IP(dst=ip_address) / ICMP(type=3, code=1)
-        send(packet, verbose=0)
-        print(f"[!] Blocked IP {ip_address}")
--- fp/detector_with_GUI/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(filename="suspicious_traffic.log", level=logging.INFO)
-
-def log_suspicious_event(ip, reason):
-    logging.info(f"Suspicious IP: {ip}, Reason: {reason}")
-    print(f"Logged suspicious activity for IP {ip}: {reason}")
